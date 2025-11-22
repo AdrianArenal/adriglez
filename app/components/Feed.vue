36a622fca37de0c2c5e55e99ca5409d510206625
Adrián Arenal <adri.arenal94@gmail.com>
2025-11-23 00:25:31 +0100

    feat: expand feed pictures

--- app/components/Feed.vue
+++ app/components/Feed.vue
@@ -26,16 +26,24 @@ const getComponentById = (id: string) => {
 
 </script>
 <template>
-    <UPageGrid class="gap-0.5 grid-cols-3 sm:grid-cols-3 lg:grid-cols-3 ">
-        <UPageCard class="rounded-none"v-for="(card, index) in global.feed" :key="index" v-bind="card" :ui="{container: card.image && 'p-0! gap-y-0 gap-x-0', wrapper: 'absolute right-0', leading: 'p-2 sm:p-4', leadingIcon:'text-white' }">
-            <template v-if="card.id" #default>
-                    <component :is="getComponentById(card.id)" v-if="getComponentById(card.id)" />
-            </template>
-            <template v-if="card.image" #default>
-                    <NuxtPicture :src="card.image" format="webp"
-                    :img-attrs="{ class: 'size-full object-cover' }"
-                    :alt="`Image ${index + 1}`" class="w-full h-full" loading="lazy" />
-            </template>
-        </UPageCard>
-    </UPageGrid>
+  <UPageGrid class="gap-0.5 grid-cols-3 sm:grid-cols-3 lg:grid-cols-3 ">
+    <UPageCard class="rounded-none" v-for="(card, index) in global.feed" :key="index" v-bind="card"
+      :ui="{ container: card.image && 'p-0! gap-y-0 gap-x-0', wrapper: 'absolute right-0', leading: 'p-2 sm:p-4', leadingIcon: 'text-white' }">
+      <template v-if="card.id" #default>
+        <component :is="getComponentById(card.id)" v-if="getComponentById(card.id)" />
+      </template>
+      <template v-if="card.image" #default>
+        <UModal fullscreen>
+          <UButton color="neutral" variant="subtle" class="size-full" :ui="{ base: 'p-0' }">
+            <NuxtPicture :src="card.image" format="webp" :img-attrs="{ class: 'size-full object-cover' }"
+              :alt="`Image ${index + 1}`" class="w-full h-full" loading="lazy" />
+          </UButton>
+          <template #body>
+            <NuxtPicture :src="card.image" format="webp" :img-attrs="{ class: 'size-full object-contain' }"
+              :alt="`Image ${index + 1}`" class="w-full h-full" loading="lazy" />
+          </template>
+        </UModal>
+      </template>
+    </UPageCard>
+  </UPageGrid>
 </template>
