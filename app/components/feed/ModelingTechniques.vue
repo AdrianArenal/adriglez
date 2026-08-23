0070fcca5788dd5636b06692b285456022f14f33
Adrián Arenal <adri.arenal94@gmail.com>
2026-08-23 17:28:14 +0200

    style: apply 'Homemade_Apple' font to section headers for consistency

--- app/components/feed/ModelingTechniques.vue
+++ app/components/feed/ModelingTechniques.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h3 class="text-2xl mb-4">Técnicas de Modelado</h3>
+    <h3 class="text-2xl mb-4 font-['Homemade_Apple']">Técnicas de Modelado</h3>
     <p class="mb-2">
       Mis habilidades en la técnica de modelado son avanzadas, lo que me permite crear prótesis y
       personajes que destacan por su realismo y originalidad.
--- app/components/feed/WorkExperience.vue
+++ app/components/feed/WorkExperience.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h3 class="text-lg font-semibold mb-4">Experiencia</h3>
+    <h3 class="text-lg font-semibold mb-4 font-['Homemade_Apple']">Experiencia</h3>
     <UUser
       v-for="(item, index) in global.workExperience"
       :key="index"
